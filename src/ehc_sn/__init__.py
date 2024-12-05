e37339796ac19bfbe76aacf8f5013c49591e5617
Borja Esteban <boressan@outlook.com>
2024-12-05 11:46:52 +0100

    add equation indexes to functions

--- src/ehc_sn/__init__.py
+++ src/ehc_sn/__init__.py
@@ -67,17 +67,17 @@ def get_sequence(items: List[Item], δ: float = 0.7) -> Sequence:
         raise ValueError("The δ value should be in [0, 1].")
     T = len(items)
     discounted = [x * δ ** (T - t) for t, x in enumerate(items, 1)]
-    return np.array(discounted).sum(axis=0)
+    return np.array(discounted).sum(axis=0)  # Eq. (2)
 
 
 def p_sequence(y: Sequence, Θ: dict[Map, float]) -> float:
     """Return the probability of a sequence."""
     a = [_p_sequence(y, θ) * p for θ, p in Θ.items()]
-    return np.array(a).sum(axis=0)
+    return np.array(a).sum(axis=0)  # Eq. (3)
 
 
 def _p_sequence(y: Sequence, θ: Map) -> float:
     """Calculate the probability of sequence given a map."""
-    lnpΘ = y @ np.log(θ)
+    lnpΘ = y @ np.log(θ)  # Eq. (5)
     # Note lnp(y|Θ_k) actually proportional to y·ln(θ_k)
     return np.exp(lnpΘ)
