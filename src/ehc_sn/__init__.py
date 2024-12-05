f8fbd10a5c6011112a582c128c3250f7621d4194
Borja Esteban <boressan@outlook.com>
2024-12-05 17:39:31 +0100

    clean model and add eq 6-9

--- src/ehc_sn/__init__.py
+++ src/ehc_sn/__init__.py
@@ -1,83 +1,90 @@
 """Sequential Navigation (SN) module for the Entorhinal–Hippocampal circuit (EHC)"""
 
 from dataclasses import dataclass
-from typing import List, Callable
+from typing import List, Tuple
 
-import torch
-from torch import nn
 import numpy as np
 import numpy.typing as npt
 
 # Type alias for integer space arrays
+Observation = npt.NDArray[np.float32]  # Observation
+Velocity = npt.NDArray[np.float32]  # Velocity
 Item = npt.NDArray[np.float32]  # Navigation Item
 Trajectory = npt.NDArray[np.float32]  # Navigation Trajectory
 Map = npt.NDArray[np.float32]  # Cognitive Map
+MapSet = dict[Map, float]  # Set of cognitive maps
 
 
 @dataclass
 class HGModelParams:
     """The param set class example for model configuration."""
 
-    items_size: int = 32
-    trajectorys_size: int = 32
-    maps_size: int = 32
+    δ: float = 0.7  # Discount factor for trajectory
+    τ: float = 0.9  # Exponential decay for mixing categorical distribution
+    c: float = 0.4  # Velocity rate for item code update
+
+    def validate(self):
+        """Validate the parameters."""
+        if not isinstance(self.δ, float) or not 0 <= self.δ <= 1:
+            raise ValueError(f"δ: {self.δ}. Must be a float between 0 and 1.")
+        if not isinstance(self.τ, float) or not 0 <= self.τ <= 1:
+            raise ValueError(f"τ: {self.τ}. Must be a float between 0 and 1.")
+        if not isinstance(self.c, float) or not 0 <= self.c <= 1:
+            raise ValueError(f"c: {self.c}. Must be a float between 0 and 1.")
 
 
-class HierarchicalGenerativeModel(nn.Module):
+class BaseModel:
+    """Base class for sequential navigation models."""
+
+    def __init__(self, shape: List[int], parameters: HGModelParams):
+        if not isinstance(parameters, HGModelParams):
+            raise TypeError("Expecting instance of HGModelParams.")
+        parameters.validate()  # Validate the parameters
+        self.parameters = parameters
+        self.ξ: Observation = np.zeros(shape, dtype=np.float32)
+        self.v: Velocity = np.zeros(shape, dtype=np.float32)
+
+    def get_trajectory(self, items: List[Item]) -> Trajectory:
+        """Return the hidden code for trajectory."""
+        T, δ = len(items), self.parameters.δ  # Extract parameters
+        discounted = [x * δ ** (T - t) for t, x in enumerate(items, 1)]
+        return np.array(discounted).sum(axis=0)  # Eq. (2)
+
+    def p_trajectory(self, y: Trajectory, Θ: dict[Map, float]) -> float:
+        """Return the probability of a trajectory."""
+        p_dist = [self.likelihood(y, θ) * p for θ, p in Θ.items()]
+        return np.array(p_dist).sum(axis=0)  # Eq. (3)
+
+    def likelihood(self, y: Trajectory, θ: Map) -> float:
+        """Calculate the likelihood of trajectory given a map."""
+        lnpΘ = y @ np.log(θ)  # Eq. (5)
+        # Note lnp(y|Θ_k) actually proportional to y·ln(θ_k)
+        return np.exp(lnpΘ)
+
+
+class HierarchicalGenerativeModel(BaseModel):
     """Model for representing hierarchical spatiotemporal data."""
 
-    def __init__(
-        self,
-        observation_size: int,
-        n_maps: int = 1,
-        config: HGModelParams | None = None,
-    ):
-        """Construct for model."""
-        config = config or HGModelParams()  # default params
-        super().__init__()
-        self.items_layer = nn.Linear(
-            in_features=observation_size,
-            out_features=config.items_size,
-            bias=True,
-        )
-        self.trajectorys_layer = nn.Linear(
-            in_features=config.items_size,
-            out_features=config.trajectorys_size,
-            bias=True,
-        )
-        self.maps_layer = nn.Linear(
-            in_features=config.trajectorys_size,
-            out_features=config.maps_size,
-            bias=True,
-        )
-        self.cluster_layer = nn.Linear(
-            in_features=config.maps_size,
-            out_features=n_maps,
-            bias=True,
-        )
+    def p_mixing(self, y: Trajectory, Θ: MapSet) -> Tuple[MapSet, Map]:
+        """Estimate the posterior mixing probability distribution."""
+        τ = self.parameters.τ  # Extract parameters
+        Θ = {θ: z**τ * self.likelihood(y, θ) ** (1 - τ) for θ, z in Θ.items()}
+        return Θ, max(Θ, key=Θ.__getitem__)  # Eq. (6) and Eq. (7)
 
-    def forward(self, observation: torch.Tensor):
+    def p_item(self, x: Item, y: Trajectory, θ: Map) -> Tuple[Item, Observation]:
+        """Estimate the posterior hidden item code."""
+        c = self.parameters.c  # Extract parameters
+        μ, Σ = self.ξ + c * self.v, self.v  # Using ξ(t-1) and v(t-1)
+        x = np.random.normal(μ, Σ) * θ - y
+        return x, np.argmax(x, axis=0)  # Eq. (8) and Eq. (9)
+
+    def inference(
+        self, x: Item, y: Trajectory, Θ: MapSet
+    ) -> Tuple[Item, Trajectory, MapSet]:
         """Inference function."""
+        Θ, θ = self.p_mixing(y, Θ)  # Eq. (6) and Eq. (7)
+        x, ξ = self.p_item(x, y, θ)  # Eq. (8) and Eq. (9)
+        self.v, self.ξ = ξ - self.ξ, ξ  # Update v(t) and ξ(t)
+        # Third estimate the posterior of the hidden trajectory code y
         raise NotImplementedError
-
-
-def get_trajectory(items: List[Item], δ: float = 0.7) -> Trajectory:
-    """Return the hidden code for trajectory."""
-    if δ < 0 or δ > 1:
-        raise ValueError("The δ value should be in [0, 1].")
-    T = len(items)
-    discounted = [x * δ ** (T - t) for t, x in enumerate(items, 1)]
-    return np.array(discounted).sum(axis=0)  # Eq. (2)
-
-
-def p_trajectory(y: Trajectory, Θ: dict[Map, float]) -> float:
-    """Return the probability of a trajectory."""
-    a = [_p_trajectory(y, θ) * p for θ, p in Θ.items()]
-    return np.array(a).sum(axis=0)  # Eq. (3)
-
-
-def _p_trajectory(y: Trajectory, θ: Map) -> float:
-    """Calculate the probability of trajectory given a map."""
-    lnpΘ = y @ np.log(θ)  # Eq. (5)
-    # Note lnp(y|Θ_k) actually proportional to y·ln(θ_k)
-    return np.exp(lnpΘ)
+        return x, y, Θ
