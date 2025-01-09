22ff2a08096a2e6a23e0437ad5297973789826a6
Borja Esteban <boressan@outlook.com>
2025-01-09 11:47:49 +0100

    fix: map likelihood cannot be reshaped

--- src/ehc_sn/utils.py
+++ src/ehc_sn/utils.py
@@ -9,16 +9,17 @@ class NeuralNetwork:
 
     def __init__(self, shape):
         self.shape = shape  # Store the shape
-        self._w = np.random.rand(*shape)
+        self._w = np.random.rand(np.prod(shape))  # Initialize weights
 
     @property
     def map(self):
-        """Cognitive map representation"""
-        # w_i = ln[θ_i] -> e^w_i = θ_i
+        """Cognitive map representation"""  # w_i = ln[θ_i]
         return np.exp(self._w).reshape(self.shape)
 
+    def __likelihood(self, y: npt.NDArray[np.float32]) -> float:
+        # Note ln[p(y|Θ_k)] actually proportional to y·ln[θ_k] = y·w
+        return y.flatten() @ self._w  # Eq. (5)
+
     def __call__(self, y: npt.NDArray[np.float32]) -> float:
-        """Calculate the likelihood of trajectory given a map."""
-        lnpΘ = y.flatten() @ self._w  # Eq. (5)
-        # Note ln[p(y|Θ_k)] actually proportional to y·ln[θ_k]
-        return lnpΘ.reshape(self.shape)
+        """Normalized likelihood of trajectory given a map."""
+        return np.exp(self.__likelihood(y))
