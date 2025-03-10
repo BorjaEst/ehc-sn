d6e48f67f9a4f3d4c694a18583a9c756088b067b
Borja Esteban <boressan@outlook.com>
2025-03-10 22:31:43 +0100

    chore: add EIParameters to EIModel

--- src/ehc_sn/model.py
+++ src/ehc_sn/model.py
@@ -1,43 +1,75 @@
 """Module for the model class."""
 
+from typing import Optional
+
 import norse.torch as snn
-from torch import nn
 import torch
+from pydantic import Field, PositiveFloat, PositiveInt
+from pydantic_settings import BaseSettings
+from torch import nn
+
+
+class EIParameters(BaseSettings):
+    """Parameters for the EI model."""
+
+    exc_size: PositiveInt = Field(
+        100, le=1000, description="Number of excitatory neurons"
+    )
+    inh_size: PositiveInt = Field(
+        100, le=1000, description="Number of inhibitory neurons"
+    )
+    rr_conn: PositiveFloat = Field(
+        0.1, le=1.0, description="Recurrent connection probability"
+    )
+    ei_prop: PositiveFloat = Field(
+        0.1, description="Weight proportion of excitatory to inhibitory"
+    )
+    ie_prop: PositiveFloat = Field(
+        0.1, description="Weight proportion of inhibitory to excitatory"
+    )
 
 
 class EIModel(nn.Module):
-    def __init__(self, e_size=1000, i_size=100):
+    """Excitatory Inhibitory Network Model."""
+
+    def __init__(self, p: Optional[EIParameters] = None) -> None:
+        self.p = p = p or EIParameters(**{})
         super().__init__()
-        self.excitatory = EIModel.excitatory_layer(e_size)
+        self.excitatory = EIModel.excitatory_layer(p)
         self.inihibitory = snn.LIFCell()
-        self.shape = (e_size, i_size)
+        self.e = self.i = None, None
         self.reset()
-        self.w_ei = 0.1 * torch.rand(e_size, i_size)
-        self.w_ie = 0.1 * torch.rand(i_size, e_size)
+        self.w_ei = p.ei_prop * torch.rand(p.exc_size, p.inh_size)
+        self.w_ie = p.ie_prop * torch.rand(p.inh_size, p.exc_size)
 
     @staticmethod
-    def excitatory_layer(e_size, rr_conn=0.1):
-        rr_matrix = torch.rand(e_size, e_size) < rr_conn
+    def excitatory_layer(p: EIParameters):
+        """Create an excitatory layer."""
+        rr_matrix = torch.rand(p.exc_size, p.exc_size) < p.rr_conn
         return snn.LIFRecurrentCell(
-            input_size=e_size,
-            hidden_size=e_size,
-            input_weights=torch.eye(e_size).type(torch.float32),
+            input_size=p.exc_size,
+            hidden_size=p.exc_size,
+            input_weights=torch.eye(p.exc_size).type(torch.float32),
             recurrent_weights=rr_matrix.type(torch.float32),
         )
 
     def reset(self):
-        self.e = self.excitatory(torch.zeros(self.shape[0]), None)
-        self.i = self.inihibitory(torch.zeros(self.shape[1]), None)
+        """Reset the state of the model."""
+        self.e = self.excitatory(torch.zeros(self.p.exc_size), None)
+        self.i = self.inihibitory(torch.zeros(self.p.inh_size), None)
 
     @property
     def e_state(self):
-        return self.e[1]  # state of the last neuron
+        """State of excitatory neurons."""
+        return self.e[1]  # State is the second element of the tuple
 
     @property
     def i_state(self):
-        return self.i[1]  # state of the last neuron
+        """State of inhibitory neurons."""
+        return self.i[1]  # State is the second element of the tuple
 
     def step(self, x):
+        """Step the model forward."""
         xe = x - self.i[0] @ self.w_ie  # feedforward inhibition
         self.e = self.excitatory(xe, self.e_state)
         xi = self.e[0] @ self.w_ei  # feedback excitation
@@ -45,10 +77,5 @@ class EIModel(nn.Module):
         return self.e[0]
 
     def forward(self, input_currents):
+        """Forward pass."""
         return torch.stack([self.step(x) for x in input_currents])
-
-
-if __name__ == "__main__":
-    model = EIModel(e_size=10)
-    input_currents = torch.rand(100, 10)  # 100 steps, 10 neurons
-    output = model(input_currents)
