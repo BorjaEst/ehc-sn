621c624fb7d3f0d718bc2a1c498183a851d60483
Borja Esteban <boressan@outlook.com>
2025-03-25 17:51:48 +0100

    feat: add encoder and decoder to model

--- src/ehc_sn/model.py
+++ src/ehc_sn/model.py
@@ -6,6 +6,7 @@ import torch
 from ehc_sn import config, parameters
 from norse.torch.functional import stdp
 from torch import nn
+import norse.torch as snn
 
 # pylint: disable=too-few-public-methods
 # pylint: disable=non-ascii-name
@@ -30,7 +31,7 @@ class Network(nn.Module, ABC):
     """The connectivity settings of the EI model."""
 
     def __init__(self, p: parameters.Network):
-        super().__init__()  # Initialize the parent class
+        super().__init__()
         self.layers = {l1: Layer(p) for l1, p in p.layers.items()}
         self.w = {l1: {l2: p.mask(l1, l2) * p.weights(l1, l2)
                        for l2 in p.synapses[l1].w}
@@ -58,3 +59,27 @@ class Network(nn.Module, ABC):
     def run(self, exc_current):
         """Run the model for a given input current."""
         return torch.stack([self(x) for x in exc_current])
+
+
+class SumDecoder(nn.Module):
+    """The decoder settings of the EI model."""
+
+    def forward(self, x):
+        """Decode the input current."""
+        return x.sum(dim=0)
+
+
+class EHCModel(nn.Module):
+    """The encoder extension of the EI model."""
+
+    def __init__(self, network: Network, seq_length: int = 32):
+        super().__init__()
+        self.encoder = snn.ConstantCurrentLIFEncoder(seq_length)
+        self.network = network
+        self.decoder = SumDecoder()
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        """Run the model for a given input current."""
+        x = self.encoder(x)
+        x = self.network.run(x)
+        return self.decoder(x)
