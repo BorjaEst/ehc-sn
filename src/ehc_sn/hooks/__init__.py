261ac50edb9dbcb5cc658af7fa900dd4bc32b923
Borja Esteban <boressa@outlook.com>
2025-06-22 22:07:18 +0200

    feat: add TqdmProgress to hooks for enhanced progress visualization

---
+++ src/ehc_sn/hooks/__init__.py
@@ -0,0 +1,9 @@
+from ehc_sn.hooks import checkpointhooks, datahooks, modelhooks
+from ehc_sn.hooks.progress_bar import TqdmProgress
+
+__all__ = [
+    "checkpointhooks",
+    "datahooks",
+    "modelhooks",
+    "TqdmProgress",
+]
