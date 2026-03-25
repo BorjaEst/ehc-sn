e27d9055c4a3a0e4302448af0bcbaf8628517cbc
Borja Esteban <boressan@outlook.com>
2026-03-25 16:34:41 +0100

    refactor(hpc): simplify imports and clean up __all__ declaration in HPC module

--- src/ehc_sn/modules/hpc/__init__.py
+++ src/ehc_sn/modules/hpc/__init__.py
@@ -1,28 +1,12 @@
 """Public API for hippocampal memory modules."""
 
-from ehc_sn.modules.hpc._base import (
-    HPCBase,
-    HPCCommonSettings,
-    HPCSensoryStepInput,
-    HPCSensoryStepOutput,
-    HPCState,
-    HPCStepInput,
-    HPCStepOutput,
-)
+from ehc_sn.modules.hpc._base import HPCSensoryStepInput, HPCSensoryStepOutput, HPCState, HPCStepInput, HPCStepOutput
 from ehc_sn.modules.hpc.modules import HPCAttention, HPCAttentionSettings, HPCAttractor, HPCAttractorSettings
 from ehc_sn.modules.hpc.query_policy import QueryPolicySettings
 
+# =================================================================================================
 __all__ = [
-    "HPCAttention",
-    "HPCAttentionSettings",
-    "HPCAttractor",
-    "HPCAttractorSettings",
-    "HPCBase",
-    "HPCCommonSettings",
-    "HPCSensoryStepInput",
-    "HPCSensoryStepOutput",
-    "HPCState",
-    "HPCStepInput",
-    "HPCStepOutput",
+    "HPCAttention", "HPCAttentionSettings", "HPCAttractor", "HPCAttractorSettings",
+    "HPCSensoryStepInput", "HPCSensoryStepOutput", "HPCState", "HPCStepInput", "HPCStepOutput",
     "QueryPolicySettings",
-]
+]  # fmt: skip
